0683503f0ac1a7d02112b646b71a937716d689f8
Marcos <marcosmaiafidelis777@outlook.com>
2022-08-11 19:13:49 -0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import styles from './App.module.css'
 
 
 const App = () => {
+
+  const [weight, setWeight] = useState(0)
+
+  const [height, setHeight] = useState(0)
+
   return(
-    <div className={styles.main}>oi</div>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          Calculadora IMC
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <input type='number'/>
+          <input type='number'/>
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
+    </div>
   )
 }
 

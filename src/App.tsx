c09517e88b7b418f2ed2a5853b0509065badbcaf
Marcos <marcosmaiafidelis777@outlook.com>
2022-08-15 10:21:37 -0300

    leftSide

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import styles from './App.module.css'
 
 
 const App = () => {
-
-  const [weight, setWeight] = useState(0)
-
-  const [height, setHeight] = useState(0)
-
+  
+  let [height, setHeight] = useState<number>(0)
+  let [weight, setWeight] = useState<number>(0)
+ 
   return(
     <div className={styles.main}>
       <header>
@@ -17,8 +16,23 @@ const App = () => {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          <input type='number'/>
-          <input type='number'/>
+          <h1>Calcule seu IMC</h1>
+          <p>O IMC (Índice de Massa Corporal) é um cálculo que ajuda a avaliar se a pessoa está dentro
+           do seu peso ideal, de acordo com a altura. </p>
+          <input 
+            placeholder='Digite sua altura'
+            type='number'
+            value={height > 0 ? height: ''}
+            onChange={e => setHeight(parseFloat(e.target.value))}
+            />
+          <input 
+            placeholder='Digite seu peso'
+            type='number'
+            value={weight > 0 ? weight: ''}
+            onChange={e => setWeight(parseFloat(e.target.value))}
+            />
+
+            <button></button>
         </div>
         <div className={styles.rightSide}>
           ...
